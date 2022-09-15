01f5e963edd400e01298936fed3e23cfebcdac7f
Dinesh Rathod <pr760677@gmail.com>
2022-09-16 02:55:15 +0530

    Env added in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    SANITY_PROJECT_ID: "7ncnu8yj",
+    SANITY_CONFIG: "production"
+  }
 }
 
 module.exports = nextConfig
